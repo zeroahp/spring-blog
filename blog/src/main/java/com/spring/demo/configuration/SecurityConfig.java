de570a314ef751b3d47bd6499c7e5da4e092b6c1
ahpzero <121105267+zeroahp@users.noreply.github.com>
2024-10-30 20:56:10 +0700

    Allow access when a valid token is provided -oauth2

--- blog/src/main/java/com/spring/demo/configuration/SecurityConfig.java
+++ blog/src/main/java/com/spring/demo/configuration/SecurityConfig.java
@@ -1,33 +1,51 @@
 package com.spring.demo.configuration;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.crypto.spec.SecretKeySpec;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig
 {
     private final String[] GET_PUBLIC_ENDPOINT = {"/api/post/**"};
-    private final String[] POST_PUBLIC_ENDPOINT = {"/api/register"};
+    private final String[] POST_PUBLIC_ENDPOINT = {"/api/register", "/auth/log-in", "/auth/introspect"};
 
-    @Bean
+    @Value("${spring.jwt.signerkey}")
+    protected String SIGNER_KEY;
+
+    @Bean //gọi phương thức khi ứng dụng khởi động và quản lý đối tượng trả về.
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeHttpRequests(request -> request.requestMatchers(HttpMethod.POST, POST_PUBLIC_ENDPOINT).permitAll()
                 .requestMatchers(HttpMethod.GET, GET_PUBLIC_ENDPOINT).permitAll()
                 .anyRequest().authenticated());
 
-        //cho phep truy cap khi cung cap token hop le
-
-
+        //Allow access when a valid token is provided.
+        //when get "/api/user/2"
+        httpSecurity.oauth2ResourceServer(oauth2 -> oauth2.jwt((jwtConfigurer -> jwtConfigurer.decoder(jwtDecoder()))));
         //tat cau hinh bao ve enpoint
         httpSecurity.csrf((AbstractHttpConfigurer::disable));
         return httpSecurity.build();
     }
 
+    @Bean
+    JwtDecoder jwtDecoder(){
+        SecretKeySpec secretKeySpec = new SecretKeySpec(SIGNER_KEY.getBytes(), "HS512");
+        return NimbusJwtDecoder
+                .withSecretKey(secretKeySpec)
+                .macAlgorithm(MacAlgorithm.HS512)
+                .build();
+    }
 }
+
