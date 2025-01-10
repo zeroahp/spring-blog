2b1d4e88182769446c0dbd2009622023dca3b868
ahpzero <121105267+zeroahp@users.noreply.github.com>
2025-01-10 21:47:05 +0700

    add statusCode

--- blog/src/main/java/com/spring/demo/enums/ErrorCode.java
+++ blog/src/main/java/com/spring/demo/enums/ErrorCode.java
@@ -1,23 +1,26 @@
 package com.spring.demo.enums;
 
-public enum ErrorCode {
-    USER_EXISTED(1001, "User existed."),
-    USERNAME_INVALID(1002, "Username must be at least 6 characters."),
-    PASSWORD_INVALID(1003, "Password must be at least 8 characters."),
-    PHONE_NUMBER_INVALID(1004, "Phone number must be at least 10 characters."),
-    TITLE_INVALID(1005, "Title must be at least 10 characters."),
-    CONTENT_INVALID(1006, "Content must be at least 10 characters."),
-    USER_NOT_EXISTED(1007, "User not existed."),
-    UNAUTHENTICATED(1008, "Unauthenticated."),
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 
+public enum ErrorCode {
+    USER_EXISTED(1001, "User existed.", HttpStatus.INTERNAL_SERVER_ERROR),
+    USERNAME_INVALID(1002, "Username must be at least 6 characters.", HttpStatus.BAD_REQUEST),
+    PASSWORD_INVALID(1003, "Password must be at least 8 characters.", HttpStatus.BAD_REQUEST),
+    PHONE_NUMBER_INVALID(1004, "Phone number must be at least 10 characters.", HttpStatus.BAD_REQUEST),
+    TITLE_INVALID(1005, "Title must be at least 10 characters.", HttpStatus.BAD_REQUEST),
+    CONTENT_INVALID(1006, "Content must be at least 10 characters.", HttpStatus.BAD_REQUEST),
+    USER_NOT_EXISTED(1007, "User not existed.", HttpStatus.NOT_FOUND),
+    UNAUTHENTICATED(1008, "Unauthenticated.", HttpStatus.UNAUTHORIZED)
     ;
     private int code;
     private String message;
+    private HttpStatusCode statusCode;
 
-
-    ErrorCode(int code, String message){
+    ErrorCode(int code, String message, HttpStatusCode statusCode) {
         this.code = code;
         this.message = message;
+        this.statusCode = statusCode;
     }
 
     public int getCode() {
@@ -27,4 +30,5 @@ public enum ErrorCode {
     public String getMessage() {
         return message;
     }
+    public HttpStatusCode getStatusCode() {return statusCode;}
 }
