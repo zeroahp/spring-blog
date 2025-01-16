5a4f6061f7e744396dffd9719316e068999359de
ahpzero <121105267+zeroahp@users.noreply.github.com>
2025-01-16 11:16:05 +0700

    custom map permission

--- blog/src/main/java/com/spring/demo/mapper/RoleMapper.java
+++ blog/src/main/java/com/spring/demo/mapper/RoleMapper.java
@@ -1,17 +1,27 @@
 package com.spring.demo.mapper;
 
 import com.spring.demo.model.dto.RoleDTO;
-import com.spring.demo.model.dto.UserDTO;
+import com.spring.demo.model.entity.PermissionEntity;
 import com.spring.demo.model.entity.RoleEntity;
-import com.spring.demo.model.entity.UserEntity;
-import com.spring.demo.model.request.RoleRequest;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface RoleMapper {
 
-    @Mapping(source = "rolePermission", target = "rolePermission")
+    @Mapping(source = "rolePermission", target = "permissionName", qualifiedByName ="mapPermissions" )
     RoleDTO toRoleDTO(RoleEntity roleEntity);
+
+    @Named("mapPermissions")
+    default Set<String> mapPermissions(Set<PermissionEntity> permissionEntities){
+        if (permissionEntities == null) {
+            return null;
+        }
+        return permissionEntities.stream().map(PermissionEntity::getPermissionName).collect(Collectors.toSet());
+    }
 
 }
